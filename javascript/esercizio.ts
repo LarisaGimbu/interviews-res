484d9486b1ada1707de9917e3141febfbea142d0
simonerocchi <s.rocchi@mind-informatica.com>
2022-03-03 11:16:33 +0100

    esercizi

---
+++ javascript/esercizio.ts
@@ -0,0 +1,35 @@
+/*
+ESERCIZIO 1:
+    Implementa la funzione "fattoriale"
+*/
+
+/**
+ * fattoriale prende come argomento un intero n
+ * e restituisce il suo fattoriale, ossia il prodotto degli interi compresi fra 1 e n.
+ * @param n l'intero di cui calcolare il fattoriale
+ */
+function fattoriale(n: number): number {
+    // scrivi qui il tuo codice
+}
+
+/*
+ESERCIZIO 2:
+    Correggere la funzione "cercaFinoA" in modo che funzioni in accordo con la sua documentazione.
+*/
+
+/**
+ * cercaFinoA cerca "corrispondenza" fra i primi "finoA" item di "lista" e restituisce la posizione
+ * del primo item corrispondente o -1 se non ne trova.
+ * @param lista array di stringhe in cui cercare
+ * @param corrispondenza stringa da cercare
+ * @param finoA numero di elementi della lista in cui cercare prima di fermarsi
+ */
+function cercaFinoA(lista: string[],corrispondenza: string, finoA: number): number {
+    for (var i = 0; i <= finoA; i ++) {
+        let item = lista[i];
+        if(item == corrispondenza) {
+            return i
+        }
+    }
+    return -1;
+}
---
+++ sql/esercizio.sql
@@ -0,0 +1,46 @@
+/*
+Date le tabelle:
+
+create table autori (
+    id int,
+    nome varchar,
+    cognome varchar,
+    anno_nascita int,
+    constraint autori_pk primary key (id)
+);
+
+create table case_editrici (
+    id int,
+    nome varchar,
+    cognome varchar,
+    nazionalita int,
+    constraint case_editrici_pk primary_key (id)
+);
+
+create table libri (
+    id int,
+    titolo varchar,
+    autore_id int,
+    casa_editrice_id int,
+    anno_pubblicazione int,
+    constraint libri_pk orimary key (id),
+    constraint libri_autori_fk foreign key (autore_id) references autori(id) on delete cascade,
+    constraint libri_case_editrici_fk foreign key (casa_editrice_id) references case_editrici(id) on delete null
+);
+*/
+
+/* ESERCIZIO 1:
+    scrivere la/le query sql per recuperare, per ogni libro scritto da un autore nato prima del 2000, tutte le informazioni riguardanti il libro e nome e cognome dell'autore.
+*/
+
+/* ESERCIZIO 2:
+    scrivere la/le istruzione sql per cancellare l'autore Jhon Doe e tutti i suoi libri.
+*/
+
+/* ESERCIZIO 3:
+    scrivere la/le istruzione sql per cancellare la casa editrice "ACME edizioni" e tutti i suoi libri.
+*/
+
+/* ESERCIZIO 4:
+    scrivere la/le query sql per recuperare, per ogni autore, nome, cognome, nazionalità e numero di libri scritti.
+*/
